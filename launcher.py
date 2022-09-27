e5d3657972f4b4c9c65b1b5f738968eb45ecb9ff
Isaglish <64142575+Isaglish@users.noreply.github.com>
2022-09-27 14:12:16 +0800

    Refactor load_config

--- launcher.py
+++ launcher.py
@@ -1,8 +1,21 @@
-from bot import Bot
+import json
+from typing import Any
+
+from bot import OddBot
+
+
+def load_config() -> dict[str, Any]:
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    return config
 
 
 def main() -> None:
-    bot = Bot()
+    bot = OddBot(
+        config=load_config(),
+        cmd_prefix="ob."
+    )
     bot.run(bot.config["discord_api_token"])
 
 
